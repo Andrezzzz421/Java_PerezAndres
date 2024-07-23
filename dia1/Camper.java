156da3b9b13c34e13db1ff76a9c06a6b57d3d54e
Andres Perez <156430410+Andrezzzz421@users.noreply.github.com>
2024-07-23 09:10:10 -0500

    feat :sparkles: modificaciones campers

--- dia1/Camper.java
+++ dia1/Camper.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package crud;
 
-/**
- *
- * @author andres
- */
 public class Camper {
+    private static int nextId = 1;
     private int id;
     private int n_documento;
     private String nombre;
@@ -21,10 +14,11 @@ public class Camper {
     private int n_celular;
     private int n_fijo;
     private String estado;
-    private String riesgo;
-    
-    public Camper(int id, int n_documento, String nombre, String nombre2, String apellido, String apellido2, String ciudad, String direccion, String acudiente, int n_celular, int n_fijo, String estado, String riesgo) {
-        this.id = id;
+    private int notaPractica;
+    private int notaTeorica;
+
+    public Camper(int n_documento, String nombre, String nombre2, String apellido, String apellido2, String ciudad, String direccion, String acudiente, int n_celular, int n_fijo) {
+        this.id = nextId++;
         this.n_documento = n_documento;
         this.nombre = nombre;
         this.nombre2 = nombre2;
@@ -35,130 +29,63 @@ public class Camper {
         this.acudiente = acudiente;
         this.n_celular = n_celular;
         this.n_fijo = n_fijo;
-        this.estado = estado;
-        this.riesgo = riesgo;
-    }
-    
-    public int getId() {
-    return id;
+        this.estado = "Inscrito";
     }
 
-    public int getN_documento() {
-        return n_documento;
+    // Getters y Setters
+    public int getId() { return id; }
+    public int getN_documento() { return n_documento; }
+    public void setN_documento(int n_documento) { this.n_documento = n_documento; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getNombre2() { return nombre2; }
+    public void setNombre2(String nombre2) { this.nombre2 = nombre2; }
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public String getApellido2() { return apellido2; }
+    public void setApellido2(String apellido2) { this.apellido2 = apellido2; }
+    public String getCiudad() { return ciudad; }
+    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public String getAcudiente() { return acudiente; }
+    public void setAcudiente(String acudiente) { this.acudiente = acudiente; }
+    public int getN_celular() { return n_celular; }
+    public void setN_celular(int n_celular) { this.n_celular = n_celular; }
+    public int getN_fijo() { return n_fijo; }
+    public void setN_fijo(int n_fijo) { this.n_fijo = n_fijo; }
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+    public int getNotaPractica() { return notaPractica; }
+    public void setNotaPractica(int notaPractica) { this.notaPractica = notaPractica; }
+    public int getNotaTeorica() { return notaTeorica; }
+    public void setNotaTeorica(int notaTeorica) { this.notaTeorica = notaTeorica; }
+
+    public void actualizarEstado() {
+        double notaIngreso = notaPractica + notaTeorica * 0.2;
+        if (notaIngreso >= 60) {
+            this.estado = "Aprobado";
+        } else {
+            this.estado = "Reprobado";
+        }
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getNombre2() {
-        return nombre2;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public String getApellido2() {
-        return apellido2;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public String getAcudiente() {
-        return acudiente;
-    }
-
-    public int getN_celular() {
-        return n_celular;
-    }
-
-    public int getN_fijo() {
-        return n_fijo;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public String getRiesgo() {
-        return riesgo;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setN_documento(int n_documento) {
-        this.n_documento = n_documento;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setNombre2(String nombre2) {
-        this.nombre2 = nombre2;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public void setApellido2(String apellido2) {
-        this.apellido2 = apellido2;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public void setAcudiente(String acudiente) {
-        this.acudiente = acudiente;
-    }
-
-    public void setN_celular(int n_celular) {
-        this.n_celular = n_celular;
-    }
-
-    public void setN_fijo(int n_fijo) {
-        this.n_fijo = n_fijo;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public void setRiesgo(String riesgo) {
-        this.riesgo = riesgo;
-    }
-    
     @Override
-        public String toString() {
+    public String toString() {
         return "Camper{id=" + id + 
                ", n_documento=" + n_documento + 
                ", nombre='" + nombre + '\'' + 
-               ", segundo nombre ='" + nombre2 + '\'' + 
+               ", nombre2='" + nombre2 + '\'' + 
                ", apellido='" + apellido + '\'' + 
-               ", segundo apellido ='" + apellido2 + '\'' + 
+               ", apellido2='" + apellido2 + '\'' + 
                ", ciudad='" + ciudad + '\'' + 
                ", direccion='" + direccion + '\'' + 
                ", acudiente='" + acudiente + '\'' + 
                ", n_celular=" + n_celular + 
                ", n_fijo=" + n_fijo + 
                ", estado='" + estado + '\'' + 
-               ", riesgo='" + riesgo + '\'' + 
+               ", notaPractica=" + notaPractica + 
+               ", notaTeorica=" + notaTeorica + 
                '}';
     }
 }
-
